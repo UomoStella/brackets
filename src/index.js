6601de48c0b72e2f2121571169627e46d89ec0b0
Valerius <vvstouba@gmail.com>
2020-10-25 00:52:54 +0300

    fix: create logik

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var brackets = [];
+
+  for (var i = 0; i < str.length; i++) {
+    for (var j = 0; j < bracketsConfig.length; j++) {      
+      if(str[i] === bracketsConfig[j][1]){
+        if(bracketsFirst[brackets.length - 1] == bracketsConfig[j][0]) {
+          bracketsFirst.pop();
+          break;
+        } else if (bracketsConfig[j][1] !== bracketsConfig[j][0]) {
+          return false;
+        }
+      }
+      if(str[i] === bracketsConfig[j][0]) {
+        brackets.push(str[i]);
+        break;
+      }
+    }
+  }
+  return brackets.length == 0;
 }
